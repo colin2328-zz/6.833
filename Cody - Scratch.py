e39a70c5607e3d49ab15e52faeb4e7467c8f44c3
Cody Coleman <colemanc@mit.edu>
2014-04-22 14:02:26 -0400

    Saving work

---
+++ Cody - Scratch.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+# Data manipulation
+import pandas as pd
+from pandas import Series, DataFrame
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+import scipy.stats as ss
+import sys
+import datetime
+import math
+
+# <codecell>
+
+
