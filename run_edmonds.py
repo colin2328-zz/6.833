be65f2dfa98f6bf7640f497d8307df946bfe5c48
colin <colin2328@gmail.com>
2014-05-03 21:06:40 -0400

    made run_edmonds use trans graph

--- run_edmonds.py
+++ run_edmonds.py
@@ -1,8 +1,59 @@
 import edmonds
+import pandas as pd
+from collections import defaultdict
 
-G = {'s':{'u':10, 'x':5}, 'u':{'v':1, 'x':2}, 'v':{'y':4}, 'x':{'u':3, 'v':9, 'y':2}, 'y':{'s':7, 'v':6}}
-root = 's'
-h = edmonds.mst(root,G)
+'''Get the graph for a KC column- usually either 'KC (Original)'' or 'KC' '''
+def get_graph(kc_column):
+	data = pd.read_csv('transactions.csv')
+
+	seq = data.loc[:, ['Anon Student Id', kc_column, 'Time', 'Outcome']]
+	seq.rename(columns = {'Anon Student Id': 'student_id',
+						  kc_column: 'kc',
+						  'Time': 'time',
+						  'Outcome': 'outcome'}, inplace = True)
+	seq = seq.sort(['student_id', 'time'])
+
+	correct_firsts = seq[seq.outcome == 'CORRECT'].groupby(['student_id', 'kc'])\
+		.apply(lambda x: x.sort('time')['time'].iloc[0])\
+		.reset_index()\
+		.rename(columns = {0: 'first_time'})\
+		.sort(['student_id', 'first_time'])
+
+	trans = {}
+	for kc in correct_firsts['kc'].unique():
+		trans[kc] = defaultdict(int)
+		
+	# Group by student_id
+	grouped = correct_firsts.groupby('student_id')
+
+	# Go through each student and count the transitions from one knowledge component to another
+	for name, group in grouped:
+		seen = set()
+		for index, row in group.iterrows():
+			for kc in seen:
+				trans[row['kc']][kc] += 1
+			seen.add(row['kc'])
+
+	return trans
+
+G = get_graph('KC (Original)')
+G = get_graph('KC')
+print G
+
+min_weight = 1e5
+min_h = None
+for root in G.keys():
+	h = edmonds.mst(root,G)
+	total_weight = 0
+	for s in h:
+		for t in h[s]:
+			total_weight += G[s][t]
+			# print "%s->%s (weight: %s)" % (s,t,G[s][t])
+	# print "total_weight", total_weight
+	if total_weight < min_weight:
+		min_weight = total_weight
+		min_h = h
+
 for s in h:
-    for t in h[s]:
-        print "%s-%s" % (s,t)
+	for t in h[s]:
+		print "%s->%s (weight: %s)" % (s,t,G[s][t])
